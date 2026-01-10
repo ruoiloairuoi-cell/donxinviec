11d2acfcd82a95d17af2d2806b7abc3346403595
ruoiloairuoi-cell <ruoiloairuoi@gmail.com>
2026-01-10 12:32:28 +0700

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.Controller;
 
-@RestController
+@Controller
 public class HelloController {
 
   @GetMapping("/")
   public String index() {
-    return "Greetings from Spring Boot!";
+    return "index";
   }
 
 }
